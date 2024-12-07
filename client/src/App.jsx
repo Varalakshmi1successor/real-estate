bc6caa02a1a17fa7153602646e469019a16cc35c
Varalakshmi1successor <2021PECCB138@panimalarengg.onmicrosoft.com>
2024-12-07 08:33:35 +0530

    Updated with new changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Home from './pages/home';
+import SignIn from './pages/signin';
+import SignUp from './pages/signup';
 import About from './pages/About';
-import Profile from './pages/Profile';
+import Profile from './pages/profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Signin() {
+export default function signin() {
   return (
     <div>S</div>
   )
